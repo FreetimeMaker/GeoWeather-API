30678ed08277895590417f2d9d44e477ec386cd1
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2026-05-05 09:44:05 +0200

    Update passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -98,6 +98,7 @@ passport.use(
   )
 );
 
-console.log("CLIENT_ID:", process.env.GITHUB_CLIENT_ID);
+console.log("GITHUB_CLIENT_ID:", process.env.GITHUB_CLIENT_ID);
+console.log("GITHUB_CLIENT_SECRET:", process.env.GITHUB_CLIENT_SECRET ? "LOADED" : "MISSING");
 
 module.exports = passport;
